56adf16ecbea528d7fb3c2685bd4f3f3803ac41a
DejaVuMan <szuszkiewiczalan928@gmail.com>
2022-10-06 16:45:50 +0200

    Updated settings.py to make use of PostgreSQL

--- Lab01/TutorialSite/TutorialSite/settings.py
+++ Lab01/TutorialSite/TutorialSite/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'TutorialSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'django_user',
+        'PASSWORD': config("PGSQL_PASSWORD"),
+        'NAME': 'db.postgresql',
     }
 }
 
