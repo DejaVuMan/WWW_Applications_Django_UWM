954f55e7d3622b1fa9e0178083f5b959323ed44d
DejaVuMan <szuszkiewiczalan928@gmail.com>
2022-10-27 15:13:48 +0200

    Utilized models.IntegerChoices, added data_dodania to  auto add timestamp of object creation

--- LabAssignments/TutorialSite/polls/models.py
+++ LabAssignments/TutorialSite/polls/models.py
@@ -24,25 +24,24 @@ class Choice(models.Model):
         return self.choice_text
 
 class Osoba(models.Model):
-
-    DATE_OPTIONS = (
-        ('1', 'January'),
-        ('2', 'February'),
-        ('3', 'March'),
-        ('4', 'April'),
-        ('5', 'May'),
-        ('6', 'June'),
-        ('7', 'July'),
-        ('8', 'August'),
-        ('9', 'September'),
-        ('10', 'October'),
-        ('11', 'November'),
-        ('12', 'December'),
-    )
+    class Dates(models.IntegerChoices):
+        JANUARY = 1
+        FEBRUARY = 2
+        MARCH = 3
+        APRIL = 4
+        MAY = 5
+        JUNE = 6
+        JULY = 7
+        AUGUST = 8
+        SEPTEMBER = 9
+        OCTOBER = 10
+        NOVEMBER = 11
+        DECEMBER = 12
 
     imie = models.CharField(max_length=64, blank=False)
     nazwisko = models.CharField(max_length=64, blank=False)
-    miesiac_urodzenia = models.CharField(max_length=2, choices=DATE_OPTIONS, default='1')
+    miesiac_urodzenia = models.CharField(max_length=2, choices=Dates.choices, default='1')
+    data_dodania = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.imie + ' ' + self.nazwisko
