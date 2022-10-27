9e7929f0425745398e83af0da94e0888616cda3c
DejaVuMan <szuszkiewiczalan928@gmail.com>
2022-10-27 15:24:33 +0200

    Added comments to top of models.py so that DB updates and migrations are easier to do for new users

--- LabAssignments/TutorialSite/polls/models.py
+++ LabAssignments/TutorialSite/polls/models.py
@@ -3,6 +3,10 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+## Creating DB updates:
+## python manage.py makemigrations CategoryModelName
+## python manage.py sqlmigrate CategoryModelName XXXX
+## python manage.py migrate
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
