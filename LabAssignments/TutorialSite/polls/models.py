e6366a46248070242d262967928e9df59c54c8f5
DejaVuMan <szuszkiewiczalan928@gmail.com>
2022-11-10 15:49:16 +0100

    Changd default value for miesiac_urodzenia in Osoba to timezone.now().month

--- LabAssignments/TutorialSite/polls/models.py
+++ LabAssignments/TutorialSite/polls/models.py
@@ -49,7 +49,7 @@ class Osoba(models.Model):
 
     imie = models.CharField(max_length=64, blank=False)
     nazwisko = models.CharField(max_length=64, blank=False)
-    miesiac_urodzenia = models.IntegerField(max_length=2, choices=Dates.choices, default=Dates.JANUARY)
+    miesiac_urodzenia = models.IntegerField(max_length=2, choices=Dates.choices, default=timezone.now().month)
     data_dodania = models.DateTimeField(auto_now_add=True)
     druzyna = models.ForeignKey(
         'Druzyna',
