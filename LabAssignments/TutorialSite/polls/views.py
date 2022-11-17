8541ca5437d015466c6dfcd7441a39f2876d1b29
DejaVuMan <szuszkiewiczalan928@gmail.com>
2022-11-17 16:30:51 +0100

    Updated views to support authentication for protected methods regarding updating or deleting Osoba data

--- LabAssignments/TutorialSite/polls/views.py
+++ LabAssignments/TutorialSite/polls/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from .models import Osoba, Druzyna
 from .serializers import OsobaModelSerializer
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django.http import HttpResponse
 
 
@@ -21,7 +23,7 @@ def person_list(request):
         return Response(serializer.data)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
+@api_view(['GET'])
 def person_detail(request, pk):
     try:
         person = Osoba.objects.get(pk=pk)
@@ -33,9 +35,18 @@ def person_detail(request, pk):
         serializer = OsobaModelSerializer(person)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+@api_view(['PUT', 'DELETE'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def person_update_delete(request, pk):
+    try:
+        person = Osoba.objects.get(pk=pk)
+    except Osoba.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
         serializer = OsobaModelSerializer(person, data=request.data)
-        if (serializer.is_valid()):
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
