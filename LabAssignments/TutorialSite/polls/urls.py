e046d7a9c0103badd65bbfa046a15e635f198dbd
DejaVuMan <szuszkiewiczalan928@gmail.com>
2022-11-17 16:35:01 +0100

    Updated pools\urls.py and TutorialSite\urls.py to support authentication

--- LabAssignments/TutorialSite/polls/urls.py
+++ LabAssignments/TutorialSite/polls/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('persons/', views.person_list, name='person_list'),
     path('persons/<int:pk>/', views.person_detail, name='person_detail'),
+    path('persons/update/<int:pk>/', views.person_update_delete, name='person_update_delete'),
+    path('persons/delete/<int:pk>/', views.person_update_delete, name='person_update_delete'),
     path('', views.index, name='index')
 ]
