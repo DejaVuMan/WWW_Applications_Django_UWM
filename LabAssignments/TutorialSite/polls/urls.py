2859299b98578d220958f4f0e9b7f54c2f23c02a
DejaVuMan <58810486+DejaVuMan@users.noreply.github.com>
2022-11-23 18:01:31 +0100

    Added URLs for views to new data

--- LabAssignments/TutorialSite/polls/urls.py
+++ LabAssignments/TutorialSite/polls/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('persons/<int:pk>/', views.person_detail, name='person_detail'),
     path('persons/update/<int:pk>/', views.person_update_delete, name='person_update_delete'),
     path('persons/delete/<int:pk>/', views.person_update_delete, name='person_update_delete'),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('teams/', views.team_list),
+    path('teams/<int:pk>/', views.team_detail)
 ]
